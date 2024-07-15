a52de38bc80092f5899a614e5ad12c7710c14306
dilipdevops5511 <dilippalani052001@gmail.com>
2024-07-15 20:00:35 +0530

    ds

--- modules/eks/kubeconfig.tf
+++ modules/eks/kubeconfig.tf
@@ -3,10 +3,6 @@ data "aws_eks_cluster_auth" "cluster" {
   name = data.aws_eks_cluster.cluster.name
 }
 
-
-data "aws_eks_cluster_auth" "cluster" {
-  name = data.aws_eks_cluster.cluster.name
-}
 output "kubeconfig_filename" {
   value = data.aws_eks_cluster_auth.cluster.kubeconfig_filename
 }
